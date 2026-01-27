362ce8aadd5d7d0f09142f66598044d7178c9b71
Radja Thaher <radjapolem@gmail.com>
2026-01-28 01:14:59 +0700

    chore(brew): update sha256

--- Formula/penpot-cli.rb
+++ Formula/penpot-cli.rb
@@ -7,7 +7,7 @@ class PenpotCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/radjathaher/penpot-cli/releases/download/v0.1.0/penpot-cli-0.1.0-darwin-aarch64.tar.gz"
-      sha256 "04d446e254f14b984dd03534f4913d73c024f128fac3c772f302ca8f2f2b3b29"
+      sha256 "d66a15f73fe84716bcf2d46650aef926be6631ecf26fd91d146d8688e121c630"
     else
       odie "penpot-cli is only packaged for macOS arm64"
     end
